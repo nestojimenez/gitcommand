fe12da7611ace38d5805557b44fdd78a5395c58d
nestojimenez <aguiladescalza@gmail.com>
2022-11-04 13:26:02 -0700

    Click button

--- java.js
+++ java.js
@@ -1,3 +1,12 @@
 alert("Hello World");
-const myHeader = document.getElementById('my_header')
+const myHeader = document.getElementById('my_header');
+const myButton = document.getElementById('my_button');
 myHeader.textContent ="My Text";
+
+myButton.addEventListener('click', myFunction);
+
+
+function myFunction(){
+    alert("You Clicked Me!");
+} 
+    
